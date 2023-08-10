686bb324875410f308f9ff18273eb162f78d2d95
Leonardo Andrey Pereira <leonardo.pereira@itajai.sc.gov.br>
2023-08-10 16:30:44 -0300

    Inserindo Botão Default (com estilos).

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { View, ScrollView, Text, Image, StyleSheet } from "react-native"
+import { useNavigation } from "@react-navigation/native";
 
 import LifeStatus from "../../Components/Common/LifeStatus";
+import DefaultButton from "../../Components/Common/DefaultButton";
 
 export default function Start() {
+
+  const handleNavAppExplanation = () => {
+    console.log("Testando o clique.")
+  }
+
   return (
     <View style={styles.container}>
       <ScrollView showVerticalScrollIndicator={false}>
@@ -16,6 +23,13 @@ export default function Start() {
           <Text style={styles.description}>
             Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o melhor nível
           </Text>
+
+          <DefaultButton
+            buttonText={"Continuar"}
+            handlePress={handleNavAppExplanation}
+            width={250}
+            height={50}
+          />
         </View>
       </ScrollView>
     </View>
