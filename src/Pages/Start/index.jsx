907977490c547ee22e60106392828a7742d98767
Leonardo Andrey Pereira <leonardo.pereira@itajai.sc.gov.br>
2023-08-11 16:52:52 -0300

    Ajuste de navegação e página Home

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -7,8 +7,10 @@ import DefaultButton from "../../Components/Common/DefaultButton";
 
 export default function Start() {
 
+  const navigation = useNavigation();
+
   const handleNavAppExplanation = () => {
-    console.log("Testando o clique.")
+    navigation.navigate("AppExplanation");
   }
 
   return (
