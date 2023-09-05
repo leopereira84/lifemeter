fea786d62b9a329f515e2df29bd7841bbea17b35
Leonardo Andrey Pereira <leonardo.pereira@itajai.sc.gov.br>
2023-09-05 14:44:58 -0300

    Renderização condicional de página e tempo de vida dinâmico

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -7,7 +7,9 @@ import StatusBar from "../../Components/Home/StatusBar";
 import CreateHabit from "../../Components/Home/CreateHabit";
 import EditHabit from "../../Components/Home/EditHabit";
 
-export default function Home() {
+import ChangeNavigationService from "../../Services/ChangeNavigationService";
+
+export default function Home({ route }) {
 
   const navigation = useNavigation();
   const [mindHabit, setMindHabit] = useState("");
@@ -15,16 +17,30 @@ export default function Home() {
   const [bodyHabit, setBodyHabit] = useState();
   const [funHabit, setFunHabit] = useState();
 
+  const [robotDaysLife, setRobotDaysLife] = useState();
+  const today = new Date();
+
   function handleNavExplanation() {
     navigation.navigate("AppExplanation")
   }
+
+  useEffect(() => {
+    ChangeNavigationService.checkShowHome(1)
+    .then((showHome) => {
+      const formDate = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
+      const checkDays = Math.trunc((new Date(formDate) - new Date(showHome.appStartData) + 1) / (1000 * 60 * 60 * 24));
+      console.log(checkDays);
+	    setRobotDaysLife(checkDays.toString().padStart(2, "0"));
+    })
+    .catch((err) => console.log(err));
+  }, [route.params]);
 
   return (
     <View style={styles.container}>
       <ScrollView>
         <View style={{ alignItems: "center" }}>
           <Text style={styles.dailyChecks}>
-            {" "}❤️ 20 dias - ✔️ 80 checks
+            {" "}❤️ {robotDaysLife} { robotDaysLife === "01" ? "dia" : "dias"} - ✔️ 80 checks
           </Text>
           <LifeStatus />
           <StatusBar />
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import AllPages from "./AllPages";
 import HomePage from "./HomePage";
+import ChangeNavigationService from "../Services/ChangeNavigationService";
 
 export default function Routes() {
-  const [showHome, setShowHome] = useState(false)
+  const [showHome, setShowHome] = useState("false");
+
+  ChangeNavigationService
+    .checkShowHome(1)
+    .then((showHome) => {
+      setShowHome(showHome.showHome);
+    })
+    .catch((err) => console.log(err));
+
   return (
     <>
-      {showHome === true ? <HomePage /> : <AllPages />}
+      {showHome === "true" ? <HomePage /> : <AllPages />}
     </>
   )
 }
